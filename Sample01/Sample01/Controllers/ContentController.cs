f740683e3d72d69cb04b8acc616415a32c9c2f98
codeback <codeback@126.com>
2018-12-07 14:46:51 +0800

    新增第一篇文章代码" git status git commit -m 新增第一篇文章代码 git commit -m 'init workspace

---
+++ Sample01/Sample01/Controllers/ContentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Sample01.Models;
+
+namespace Sample01.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly Content contents;
+        public ContentController(IOptions<Content> options)
+        {
+            contents = options.Value;
+        }
+        public IActionResult Index()
+        {
+            return View(new ContentViewModel { Contents = new List<Content>{ contents }});
+            //var contents = new List<Content>();
+
+            //for(int i=0;i<11;i++)
+            //{
+            //    contents.Add(new Content { Id = i, Title = $"{i}的标题", content=$"{i}的内容", Status=1, Add_Time=DateTime.Now});
+            //}
+            //return View(new ContentViewModel { Contents = contents });
+        }
+    }
+}
---
+++ Sample01/Sample01/Models/Content.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample01.Models
+{
+    public class Content
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string content { get; set; }
+
+        public int Status { get; set; }
+        public DateTime Add_Time { get; set; }
+        public DateTime Modify_Time { get; set; }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+    }
+}
